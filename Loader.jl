3457474023b4df809cb6ff81a2f725124cbda16b
dayu11 <yudakuai18@163.com>
2018-12-11 17:30:47 +0800

    Change the format of coding
    Please note the change of method's name

--- Loader.jl
+++ Loader.jl
@@ -1,114 +1,126 @@
-#using Flux: onehotbatch, onecold
 using Random
 using DelimitedFiles
 using ResumableFunctions
-##n*m -> n*m*class_num
-function Onehot(array, class_num, idx)
-    n=size(array)[1]
-    m=size(array)[2]
-    result=zeros(Float32, (n, idx, class_num))
-    for i in 1:n
-        for j in 1:idx
-            result[i, j, array[i, j]]=1
+
+
+"""
+n*m -> n*m*class_num
+"""
+
+function onehot(Array, ClassNum, Idx)
+    N = size(Array)[1]
+    M = size(Array)[2]
+    Result = zeros(Float32, (N, Idx, ClassNum))
+    for i in 1:N
+        for j in 1:Idx
+            Result[i, j, Array[i, j]] = 1
         end
     end
-    return result
+    return Result
 end
 
-#this generator returns one epoch of Minibatches
-#steps=Int(num_examples/batch_size)
-@resumable function One_Epoch(data_dict, batch_size, num_words, num_labels) :: Tuple
-    num_examples=size(data_dict['x'])[1]
-    steps=convert(Int, floor(num_examples/batch_size))
-    random_idx=Random.randperm(num_examples)
 
-    for i in 1:steps
-        mini_idx=random_idx[(i-1)*batch_size + 1:i*batch_size]
-        mini_x=data_dict['x'][mini_idx,:]
-        row_sum=sum(mini_x, dims=1)
-        n1=size(row_sum)[1]
-        m1=size(row_sum)[2]
-        idx=m1
-        null=2*batch_size
-        #print(row_sum)
-        for i in 0:m1-1
-            if(row_sum[n1, idx]!=null)
+"""
+this generator returns one epoch of Minibatches
+Steps = Int(NumExamples/BatchSize)
+"""
+
+@resumable function one_epoch(DataDict, BatchSize, NumWords, NumLabels) :: Tuple
+    NumExamples = size(DataDict['x'])[1]
+    Steps = convert(Int, floor(NumExamples / BatchSize))
+    RandomIdx = Random.randperm(NumExamples)
+
+    for i in 1:Steps
+        MiniIdx = RandomIdx[(i - 1) * BatchSize + 1:i * BatchSize]
+        MiniX = DataDict['x'][MiniIdx,:]
+        RowSum = sum(MiniX, dims = 1)
+        N1 = size(RowSum)[1]
+        M1 = size(RowSum)[2]
+        Idx = M1
+        Null = 2 * BatchSize
+        for i in 0 : M1 - 1
+            if RowSum[N1, Idx] != Null
                 break
             end
-            idx-=1
+            Idx -= 1
         end
-        #print(idx, '\n')
-
-        @yield (Onehot(data_dict['x'][mini_idx,:], num_words, idx), Onehot(data_dict['y'][mini_idx,:], num_labels, idx))
+        @yield (onehot(DataDict['x'][MiniIdx,:], NumWords, Idx), onehot(DataDict['y'][MiniIdx,:], NumLabels, Idx))
     end
 end
 
 
-#num_words means how many words in our dict, num_labels means how many labels
-@resumable function Minibatches(data_dict, batch_size, num_words, num_labels, steps) :: Tuple
-    num_examples=size(data_dict['x'])[1]
-    for i in 1:steps
-        random_idx=Random.randperm(num_examples)[1:batch_size]
-        mini_x=data_dict['x'][random_idx,:]
-        row_sum=sum(mini_x, dims=1)
-        n1=size(row_sum)[1]
-        m1=size(row_sum)[2]
-        idx=m1
-        null=2*batch_size
-        #print(row_sum)
-        for i in 0:m1-1
-            if(row_sum[n1, idx]!=null)
+"""
+NumWords means how many words in our dict, NumLabels means how many labels
+"""
+
+@resumable function minibatches(DataDict, BatchSize, NumWords, NumLabels, Steps) :: Tuple
+    NumExamples = size(DataDict['x'])[1]
+    for i in 1:Steps
+        RandomIdx = Random.randperm(NumExamples)[1:BatchSize]
+        MiniX = DataDict['x'][RandomIdx,:]
+        RowSum = sum(MiniX, dims = 1)
+        N1 = size(RowSum)[1]
+        M1 = size(RowSum)[2]
+        Idx = M1
+        Null = 2 * BatchSize
+        for i in 0 : M1 - 1
+            if RowSum[N1, Idx] != Null
                 break
             end
-            idx-=1
+            Idx -= 1
         end
-        #print(idx, '\n')
-
-        @yield (Onehot(data_dict['x'][random_idx,:], num_words, idx), Onehot(data_dict['y'][random_idx,:], num_labels, idx))
+        @yield (onehot(DataDict['x'][RandomIdx,:], NumWords, Idx), onehot(DataDict['y'][RandomIdx,:], NumLabels, Idx))
     end
 end
 
-#function Readfile(training_file, testing_file, min_freq=10)
-function Readfile()
-    #special symbols
-    #EOF:1
-    #pad:2
-    #less than min frequence:3
-    training_dict=Dict() ##containg two arrays : 'x' contains featuers, 'y' contains labels
-    testing_dict=Dict()
-    ##please note the index in julia strating at 1, the same as matlab.
-    ##a=[1, 2]. a[1]=1 and a[2]=2
-    training_dict['x']=DelimitedFiles.readdlm("trn_x.txt", ' ', Int)
-    training_dict['y']=DelimitedFiles.readdlm("trn_y.txt", ' ', Int)
-    testing_dict['x']=DelimitedFiles.readdlm("test_x.txt", ' ', Int)
-    testing_dict['y']=DelimitedFiles.readdlm("test_y.txt", ' ', Int)
 
-    word_dict=Dict() ##map interger to word
-    label_dict=Dict() ##map interger to label
+function read_file()
+
+    """
+    special symbols
+    EOF:1
+    pad:2
+    less than min frequence:3
+
+    containg two Arrays : 'x' contains featuers, 'y' contains labels
+    please note the index in julia strating at 1, the same as matlab.
+    a=[1, 2]. a[1]=1 and a[2]=2
+    """
+
+    TrainingDict = Dict()
+    TestingDict = Dict()
+    TrainingDict['x'] = DelimitedFiles.readdlm("trn_x.txt", ' ', Int)
+    TrainingDict['y'] = DelimitedFiles.readdlm("trn_y.txt", ' ', Int)
+    TestingDict['x'] = DelimitedFiles.readdlm("test_x.txt", ' ', Int)
+    TestingDict['y'] = DelimitedFiles.readdlm("test_y.txt", ' ', Int)
+
+    WordDict = Dict()
+    LabelDict = Dict()
     open("word_dict.txt") do f
         for l in eachline(f)
-            arr=split(l)
-            word_dict[arr[1]]=parse(Int, arr[2])
+            Arr = split(l)
+            WordDict[Arr[1]] = parse(Int, Arr[2])
         end
     end
     open("label_dict.txt") do f
         for l in eachline(f)
-            arr=split(l)
-            label_dict[arr[1]]=parse(Int, arr[2])
+            Arr = split(l)
+            LabelDict[Arr[1]] = parse(Int, Arr[2])
         end
     end
-    #print(word_dict["EOF"])
-    return training_dict, testing_dict, word_dict, label_dict
+    return TrainingDict, TestingDict, WordDict, LabelDict
 end
 
-# batch_size=10000
-#
-# training_dict, testing_dict, word_dict, label_dict = Readfile()
-# generator=One_Epoch(training_dict, batch_size, length(word_dict), length(label_dict))
-# for arr in generator
-#     mini_trn_x=arr[1]
-#     mini_trn_y=arr[2]
-#
-#     print("size of input x matrix: ", size(mini_trn_x), " it has type", typeof(mini_trn_x), '\n', "size of input y matrix: ", size(mini_trn_y), " it has type", typeof(mini_trn_y), '\n')
-#
-# end
+"""
+ BatchSize = 100
+
+ TrainingDict, TestingDict, WordDict, LabelDict = read_file()
+ generator = one_epoch(TrainingDict, BatchSize, length(WordDict), length(LabelDict))
+ for Arr in generator
+     mini_trn_x = Arr[1]
+     mini_trn_y = Arr[2]
+
+     print("size of input x matrix: ", size(mini_trn_x), " it has type", typeof(mini_trn_x), '\n', "size of input y matrix: ", size(mini_trn_y), " it has type", typeof(mini_trn_y), '\n')
+
+ end
+"""
