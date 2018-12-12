6d823e38b268980f6201de3c852f71e2852c34ad
s1530129650 <1530129650@qq.com>
2018-12-12 09:37:41 +0800

    specification

--- evaluate_final.jl
+++ evaluate_final.jl
@@ -103,7 +103,7 @@ function countChunks(trueSeqs, predictSeqs)
         prevTrueTag = trueTag
         prevPredTag  = predTag
     end
-
+find
     if startFlage == true
         evaluate[1] += 1
     end
@@ -121,3 +121,4 @@ predictSeqs = [1 2 1 2 1 ;2 2 2 2 2 ]'
 
 f1 = countChunks(trueSeqs,predictSeqs)
 println(f1)
+S
