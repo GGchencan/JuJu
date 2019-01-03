52bc5ef23d640cff425d0a2887200e9c1cc741bf
Yufei Xu <v-yufxu@microsoft.com>
2019-01-03 19:01:28 +0800

    fix bugs in data_preprocess

--- demo/data_preprocess_custom.jl
+++ demo/data_preprocess_custom.jl
@@ -3,6 +3,7 @@ this file is used for the costumer to retrain this model with their own data
     the data format: please give the data according to the conll2003 data format
     the arguments: the first argument(must) is the path to the train data
                    the second argument(optional) is the path to the test data
+                   the third is the path to the dev data
 """
 
 include("../Data/preprocess_helper.jl")
@@ -37,7 +38,7 @@ if(length(args) == 3)
     test_path = args[2]
     test_mat_x, test_mat_y = construct_test_data(test_path, word_dict, label_dict, stop_word, max_seq_len)
     eval_path = args[3]
-    eval_mat_x, eval_mat_y = construct_test_data(test_path, word_dict, label_dict, stop_word, max_seq_len)
+    eval_mat_x, eval_mat_y = construct_test_data(eval_path, word_dict, label_dict, stop_word, max_seq_len)
     save_data(word_dict, label_dict, trn_mat_x, trn_mat_y, test_mat_x, test_mat_y, eval_mat_x, eval_mat_y)
 else
     save_data(word_dict, label_dict, trn_mat_x, trn_mat_y, nothing, nothing, nothing, nothing)
