da9e9c270bff35c545778d9c47298aa01e14dca2
teslacool <1462540095@qq.com>
2019-01-14 11:42:25 +0800

    lstm gard check and load random embeddings for words not in glove

--- test/unittest.jl
+++ test/unittest.jl
@@ -16,7 +16,26 @@ include("../Dense_m.jl")
     end
     @test layer(input_data) == expected_output
 end
-
+using Flux
+using Flux.Tracker
+include("../lstm_custom.jl")
 @testset "MyBiLSTM test" begin
-    @test true
+    seq_len = 10
+    batch = 3
+    in_dim = 20
+    out_dim  = 30
+    x = rand(batch,seq_len,  in_dim)
+    y = ones(batch,seq_len,  2 * out_dim)
+    bilstm = MyBiLSTM(in_dim, out_dim)
+    ps = params(bilstm)
+    loss(x, y) = Flux.mse(bilstm(x), y)
+    l1 = loss(x, y )
+    Tracker.back!(l1)
+    grad1 =  ps[1].grad[1,1]
+    ps[1].data[1,1] += 0.000001
+    l2 = loss(x, y )
+    grad2 = (l2 - l1) / 0.000001
+    grad2 = Tracker.data(grad2)
+    @test isapprox(abs(grad2), abs(grad1); atol=0.000001)
+    # @test abs(grad2) ≈ abs(grad1)
 end
